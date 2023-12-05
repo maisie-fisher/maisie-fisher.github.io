11850e031cac32578b994cf72bff7fb4f4f12d93
Maisie Fisher <margretvfisher@gmail.com>
2023-12-05 23:31:10 +0800

    adding map

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-/*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    2.4 - Adding SVGs with D3
-*    d3.select('rect') -> selects first rectangle on the page
-*    d3.select('#center') -> selects rectangle with id = 'center'
-*    d3.selectAll('rect') -> selects all rectangles on the page
-*    d3.selectAll('.outside) -> selects rectangles with class = 'outside'
-*    d3.select().append() -> adds SVG as child
-*/
-
 const svg = d3.select("#chart-area").append("svg")
   .attr("width", 400)
   .attr("height", 400)
+
+singaporeOutline = FileAttachment("../data/NationalMapPolygonKML.geojson").json()
+
+console.log(singaporeOutline)
+
+// Plot.plot({
+//   marks: [
+//     Plot.geo(singaporeMainland, {
+//       fill: (d) => '#FAF8ED',
+//       stroke: (d) => 'black'
+//     }),
+//     Plot.geo(parks, {
+//       fill: (d) => '#748E63',
+//       stroke: (d) => 'black'
+//     })
+//     ]
+// })
 
 svg.append("circle")
   .attr("cx", 100)
